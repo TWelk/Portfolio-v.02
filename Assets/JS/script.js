e284c957f51c038c71ee6a70dcbcb3c26738da2f
Tyler Welker <85715276+TWelk@users.noreply.github.com>
2022-02-14 15:45:20 -0500

    Update script.js
    removing unused code

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -89,8 +89,6 @@ prevbtn.addEventListener('click', prevSlide, false);
 setInterval(function() {
         if (slideShow.matches(':hover')) {
             clearInterval()
-        } else {
-            nextSlide()
         }
     },
     3000);
